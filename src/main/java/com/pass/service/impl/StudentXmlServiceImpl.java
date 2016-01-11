39aa1dd96e5e6bcea99562e8bcd084d7219afb67
Andrei Lescu <andrei.lescu@dialogdata.de>
2016-01-11 12:23:13 +0200

    [Clean up]. Added xml-database profile to StudentXmlServiceImpl.

--- src/main/java/com/pass/service/impl/StudentXmlServiceImpl.java
+++ src/main/java/com/pass/service/impl/StudentXmlServiceImpl.java
@@ -3,18 +3,15 @@ package com.pass.service.impl;
 import java.io.IOException;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.oxm.castor.CastorMarshaller;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import com.pass.XmlConverter;
 import com.pass.model.StudentDO;
 import com.pass.service.api.StudentXmlService;
 
-
+@Profile("xml-database")
 @Service
 public class StudentXmlServiceImpl implements StudentXmlService {
 	
